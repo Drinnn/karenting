8f21e67fa044aa7378ff1beb8fa9cfb3f8ee5de9
Drinnn <pedro_lontro@hotmail.com>
2022-02-02 22:24:56 -0300

    chore: add tailwind configuration fiels

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false,
+  content: [],
+  theme: {
+    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+  },
+  plugins: [],
+};
